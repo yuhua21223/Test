5dd8086af4e75a71d8bbf10a0415ebf6ae9f55bd
yuhua21223 <yuhua21223@gmail.com>
2019-12-11 18:18:28 -0800

    Project

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+
+// dbPassword = 'mongodb+srv://yuhua21223:'+ encodeURIComponent('Mcdonald21223!') + '@cluster-0ikw41.mongodb.net/test?retryWrites=true';
+
+
+//
+dbPassword = 'mongodb+srv://yuhua21223:'+ encodeURIComponent('Mcdonald21223!') + '@cluster0-ikw41.mongodb.net/test?retryWrites=true';
+
+
+//mongodb+srv://yuhua21223:<password>@cluster0-ikw41.mongodb.net/test?retryWrites=true&w=majority
+
+
+module.exports = {
+    mongoURI: dbPassword
+};
